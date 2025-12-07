5fb1b1b476cf51a984fb719e3e8c2d292f4e69e8
Vitthal Humbe <vitthal.3454@gmail.com>
2025-12-07 16:33:19 +0530

    minor changes

--- lib/services/progress_service.dart
+++ lib/services/progress_service.dart
@@ -1,6 +1,5 @@
 // lib/services/progress_service.dart
 import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:firebase_auth/firebase_auth.dart';
 
 class ProgressService {
   final FirebaseFirestore _db = FirebaseFirestore.instance;
